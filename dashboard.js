20bff7e4dbd12d7ae3fe01516f023c21f4e4e69b
facundolunam <52012997+facundolunam@users.noreply.github.com>
2023-08-04 16:21:36 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -47,4 +47,37 @@ const parkingRatios = {
 
 function calculateParking() {
     // Get development type from radio buttons
-    const developmentType =
+    const developmentType = document.querySelector('input[name="developmentType"]:checked').value;
+
+    // Get apartment counts from input fields
+    const count1Bedroom = parseInt(document.getElementById("count1Bedroom").value);
+    const count2Bedroom = parseInt(document.getElementById("count2Bedroom").value);
+    const count3PlusBedroom = parseInt(document.getElementById("count3PlusBedroom").value);
+
+    // Calculate estimated parking for each source and apartment type
+    const rsisParking = (count1Bedroom * parkingRatios[developmentType].rsis[1]) +
+        (count2Bedroom * parkingRatios[developmentType].rsis[2]) +
+        (count3PlusBedroom * parkingRatios[developmentType].rsis["3+"]);
+
+    const rutgersSurveyParking = (count1Bedroom * parkingRatios[developmentType].rutgersSurvey[1]) +
+        (count2Bedroom * parkingRatios[developmentType].rutgersSurvey[2]) +
+        (count3PlusBedroom * parkingRatios[developmentType].rutgersSurvey["3+"]);
+
+    const detailedRutgersSurveyParking = (count1Bedroom * parkingRatios[developmentType].detailedRutgersSurvey[1]) +
+        (count2Bedroom * parkingRatios[developmentType].detailedRutgersSurvey[2]) +
+        (count3PlusBedroom * parkingRatios[developmentType].detailedRutgersSurvey["3+"]);
+
+    const censusDataParking = (count1Bedroom * parkingRatios[developmentType].censusData[1]) +
+        (count2Bedroom * parkingRatios[developmentType].censusData[2]) +
+        (count3PlusBedroom * parkingRatios[developmentType].censusData["3+"]);
+
+    // Display the results on the dashboard
+    const resultContainer = document.getElementById("resultContainer");
+    resultContainer.innerHTML = `
+        <h3>${developmentType === "garden" ? "Garden" : "Highrise"} Development Type:</h3>
+        <p>RSIS Ratio: ${rsisParking} parking spaces</p>
+        <p>Rutgers Parking Survey Ratio: ${rutgersSurveyParking} parking spaces</p>
+        <p>Detailed Rutgers Parking Survey Ratio: ${detailedRutgersSurveyParking} parking spaces</p>
+        <p>Census data (ACS) Ratio: ${censusDataParking} parking spaces</p>
+    `;
+}
