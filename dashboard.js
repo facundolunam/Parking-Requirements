c1c48e587cadfc4e387e65c5e9f5f29efff15d05
facundolunam <52012997+facundolunam@users.noreply.github.com>
2023-08-04 15:47:39 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,8 @@
 const parkingRatios = {
-    1: 1.5,
-    2: 2.0,
-    3: 2.5
+    rsis: 1.5,
+    rutgersSurvey: 2.0,
+    detailedRutgersSurvey: 2.5,
+    censusData: 2.8
 };
 
 function calculateParking() {
@@ -10,12 +11,28 @@ function calculateParking() {
     const count2Bedroom = parseInt(document.getElementById("count2Bedroom").value);
     const count3PlusBedroom = parseInt(document.getElementById("count3PlusBedroom").value);
 
-    // Calculate estimated parking
-    const estimatedParking = (count1Bedroom * parkingRatios[1]) +
-        (count2Bedroom * parkingRatios[2]) +
-        (count3PlusBedroom * parkingRatios[3]);
+    // Calculate estimated parking for each ratio
+    const rsisParking = (count1Bedroom * parkingRatios.rsis) +
+        (count2Bedroom * parkingRatios.rsis) +
+        (count3PlusBedroom * parkingRatios.rsis);
 
-    // Display the result on the dashboard
-    document.getElementById("estimatedParking").textContent = `${estimatedParking} parking spaces`;
+    const rutgersSurveyParking = (count1Bedroom * parkingRatios.rutgersSurvey) +
+        (count2Bedroom * parkingRatios.rutgersSurvey) +
+        (count3PlusBedroom * parkingRatios.rutgersSurvey);
+
+    const detailedRutgersSurveyParking = (count1Bedroom * parkingRatios.detailedRutgersSurvey) +
+        (count2Bedroom * parkingRatios.detailedRutgersSurvey) +
+        (count3PlusBedroom * parkingRatios.detailedRutgersSurvey);
+
+    const censusDataParking = (count1Bedroom * parkingRatios.censusData) +
+        (count2Bedroom * parkingRatios.censusData) +
+        (count3PlusBedroom * parkingRatios.censusData);
+
+    // Display the results on the dashboard
+    document.getElementById("rsisResult").textContent = `RSIS Ratio: ${rsisParking} parking spaces`;
+    document.getElementById("rutgersSurveyResult").textContent = `Rutgers Parking Survey Ratio: ${rutgersSurveyParking} parking spaces`;
+    document.getElementById("detailedRutgersSurveyResult").textContent = `Detailed Rutgers Parking Survey Ratio: ${detailedRutgersSurveyParking} parking spaces`;
+    document.getElementById("censusDataResult").textContent = `Census data (ACS) Ratio: ${censusDataParking} parking spaces`;
 }
+
  
