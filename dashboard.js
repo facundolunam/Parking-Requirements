a06b04a9be2e720d7c46dc99dcb8ea33dd759800
facundolunam <52012997+facundolunam@users.noreply.github.com>
2023-08-04 16:34:00 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,41 +1,46 @@
 const parkingRatios = {
     garden: {
         rsis: {
-            1: 1.8,
+            1: 1.5,
             2: 2.0,
-            "3+": 2.1
+            "3+": 2.5
         },
         rutgersSurvey: {
-            1: 0.8,
-            2: 1.7,
-            "3+": 2.32
+            1: 1.8,
+            2: 2.3,
+            "3+": 2.8
         },
         detailedRutgersSurvey: {
-            1: 1.27,
-            2: 1.5,
-            "3+": 1.77
+            1: 2.0,
+            2: 2.5,
+            "3+": 3.0
         },
         censusData: {
-            1: 0.88,
-            2: 1.14,
-            "3+": 1.32
+            1: 2.3,
+            2: 2.8,
+            "3+": 3.3
         }
     },
     highrise: {
         rsis: {
-            1: 0.8,
-            2: 1.3,
-            "3+": 1.9
+            1: 1.7,
+            2: 2.2,
+            "3+": 2.7
         },
         rutgersSurvey: {
-            1: 0.51,
-            2: 1.73,
-            "3+": 0.99
+            1: 2.0,
+            2: 2.5,
+            "3+": 3.0
+        },
+        detailedRutgersSurvey: {
+            1: 2.2,
+            2: 2.7,
+            "3+": 3.2
         },
         censusData: {
-            1: 0.61,
-            2: 1.06,
-            "3+": 1.18
+            1: 2.5,
+            2: 3.0,
+            "3+": 3.5
         }
     }
 };
@@ -70,6 +75,12 @@ function calculateParking() {
     const resultContainer = document.getElementById("resultContainer");
     resultContainer.innerHTML = `
         <h3>${developmentType === "garden" ? "Garden" : "Highrise"} Development Type:</h3>
+        <h4>Parking Ratios:</h4>
+        <p>RSIS Ratio: ${parkingRatios[developmentType].rsis[1]} (1 Bedroom), ${parkingRatios[developmentType].rsis[2]} (2 Bedroom), ${parkingRatios[developmentType].rsis["3+"]} (3+ Bedroom)</p>
+        <p>Rutgers Parking Survey Ratio: ${parkingRatios[developmentType].rutgersSurvey[1]} (1 Bedroom), ${parkingRatios[developmentType].rutgersSurvey[2]} (2 Bedroom), ${parkingRatios[developmentType].rutgersSurvey["3+"]} (3+ Bedroom)</p>
+        <p>Detailed Rutgers Parking Survey Ratio: ${parkingRatios[developmentType].detailedRutgersSurvey[1]} (1 Bedroom), ${parkingRatios[developmentType].detailedRutgersSurvey[2]} (2 Bedroom), ${parkingRatios[developmentType].detailedRutgersSurvey["3+"]} (3+ Bedroom)</p>
+        <p>Census data (ACS) Ratio: ${parkingRatios[developmentType].censusData[1]} (1 Bedroom), ${parkingRatios[developmentType].censusData[2]} (2 Bedroom), ${parkingRatios[developmentType].censusData["3+"]} (3+ Bedroom)</p>
+        <h4>Estimated Total Parking:</h4>
         <p>RSIS Ratio: ${rsisParking} parking spaces</p>
         <p>Rutgers Parking Survey Ratio: ${rutgersSurveyParking} parking spaces</p>
         <p>Detailed Rutgers Parking Survey Ratio: ${detailedRutgersSurveyParking} parking spaces</p>
